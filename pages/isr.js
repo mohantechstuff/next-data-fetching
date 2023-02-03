4bcd8115c47324ae68349b0d7e67c258bce54a70
mohantechstuff <84971837+mohantechstuff@users.noreply.github.com>
2023-02-03 23:52:25 +0530

    Update isr.js

--- pages/isr.js
+++ pages/isr.js
@@ -5,7 +5,7 @@ export default function IncrementalStaticRegeneration({ data }) {
   return (
     <>
       {data.map((e) => (
-        <h2 key={e.id}>{e.name}</h2>
+        <h2 key={e.id}>{e.title}</h2>
       ))}
     </>
   );
